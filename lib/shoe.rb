aabba049c565348bf929ad22a6965c2d169a733f
Junaid Malik <jmalik981@gmail.com>
2019-11-13 04:11:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,14 @@
 # Make your shoe class here!
 class Shoe 
+  attr_accessible :color, :size, :material, :condition
+  attr_reader :brand
   
+  def initialization(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
